4a0792efe7080d3d6fc0967d24ed76787754ecff
linyehmonographic <linyehmonographic@gmail.com>
2017-10-08 15:22:26 +0800

    update UGM.bat

--- Windows/UGM.bat
+++ Windows/UGM.bat
@@ -1,5 +1,5 @@
 adb shell mkdir /sdcard/UGM
 adb logcat -c
 adb logcat -s Unity | UGM_LogWriter.exe
-::adb pull /sdcard/UGM
-::#CRASH#adb shell -rf /sdcard/UGM
+adb pull /sdcard/UGM
+adb shell rm -rf /sdcard/UGM
